44335701979bd7164ecd46526190972463258b32
Tracy Goznikar <tracygoznikar@gmail.com>
2020-12-16 17:29:37 -0600

    adds changes from tutor session to show plot and fix ticks

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,16 +1,16 @@
 //1. Use the D3 library to read in samples.json.
 //Fetch JSON data and console.log it
-d3.json("samples.json").then(function(data) {
-    console.log(data);
-});
-const dataPromise = d3.json("samples.json");
-console.log("Data Promise: ", dataPromise)
-function unpack(rows, index) {
-    return rows.map(function(row) {
-        return row[index];
-    });
-}
-//2.Dropdown #1
+// d3.json("samples.json").then(function(data) {
+//     console.log(data);
+// });
+// const dataPromise = d3.json("samples.json");
+// console.log("Data Promise: ", dataPromise)
+// function unpack(rows, index) {
+//     return rows.map(function(row) {
+//         return row[index];
+//     });
+// }
+
 //(#selDataset)
 //Create init 
 function init() {
@@ -24,48 +24,50 @@ function init() {
         })
         //build chart with first id name
         buildPlot(sampleIDNames[0]);
+        
     })
+   
 }
-init();
-
-//Event listener function #2
+//event when option is changed
 function optionChanged(newSample) {
-    //recall function inside option change (buildPlot(neewsample))
+    //buildplot function
         buildPlot(newSample);
     };
-    //3. Create a horizontal bar chart with a dropdown menu 
-    //to display the top 10 OTUs found in that individual.
-    //Unpack JSON
-    //Building plot by feeding the sample id value from above init function #3
+    //create bar chart
     function buildPlot(sample) {
         d3.json("samples.json").then((data) => {
-            //Filer function matching the id
             var samples = data.samples 
             var filteredData = samples.filter(sampleID => sampleID.id === sample)[0]
-            //Use sample_values as the values for the bar chart.
+            //sample values
             var sample_values = filteredData.sample_values;
-            //Use otu_ids as the labels for the bar chart.
+            var sample_values = sample_values.slice(0,10).reverse();
+            //otu_ids
             var otu_ids = filteredData.otu_ids;
-            //Use otu_labels as the hovertext for the chart.
+            var otu_ids = otu_ids.slice(0,10).reverse();
+            //otu_labels 
             var otu_labels = filteredData.otu_labels;
+            var otu_labels = otu_labels.slice(0,10).reverse();
+            
             console.log(sample_values);
             console.log(otu_ids);
             console.log(otu_labels);
             //trace, layout go here *****USE SLICE & REVERSE to display top 10 OTUs
             let trace1 = {
                 x: sample_values,
-                y: otu_ids,
+                y: otu_ids.map(data=>`otu_ids ${data}`),
                 text: otu_labels,
-                type: 'bar',
-                orientation: 'h'
+                type: "bar",
+                orientation: "h"
             };
+
+            let data1 = [trace1];
+
             let layout = {
                 xaxis: { title: "Sample Values" },
                 yaxis: { title: "OTU IDs" }
             };
-            Plotly.NewPlot("bar",trace1, layout);
+            Plotly.newPlot("bar",data1, layout);
         }) 
-    }
-    //buildPlot();
-    //id directly
-    //Plotly.NewPlot("bar",trace1, layout);
+    };
+
+    init();
